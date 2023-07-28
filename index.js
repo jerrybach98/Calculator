c0e38e1b052970075eeab0cbb409f4f7ebe2a6cf
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2023-07-28 01:05:03 -0700

    setup files, basic operator functions, and footer

---
+++ index.js
@@ -0,0 +1,20 @@
+function add (a,b) {
+	return a + b;
+};
+console.log(add(5,3))
+
+function subtract (a,b) {
+	return a - b;
+};
+console.log(subtract(5,3))
+
+function multiply (a,b) {
+  return a * b;
+};
+console.log(multiply(5,3))
+
+
+function divide (a,b) {
+  return a / b;
+};
+console.log(divide(5,3))
