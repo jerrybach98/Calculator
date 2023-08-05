be9271da62fa754ae6e614ce1a7088c31aca38c8
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2023-08-04 23:42:13 -0700

    add function to toggle operators buttons, fix equal button interactions, and delete button functional
    Equals button will only run operate function when two number variables have value. Delete button has rewinding effect by slicing string until empty then redisplaying previous inputted value and toggled operator.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function divide (a,b) {
   return a / b;
 };
 
-//Takes string inputs from variables, sets themn to numbers, then operates on them
+//Takes string inputs from variables, sets themn to numbers, then operates on the two given numbers
 function operate () {
   const a = Number(firstNum);
   const b = Number(secNum);
@@ -49,27 +49,30 @@ const currentDisplay = document.querySelector(".currentDisplay");
 const operator = document.querySelectorAll(".operator");
 const equals = document.querySelector("#equals");
 const allClear = document.getElementById("allClear");
-const del = document.getElementById("delete");
+const del = document.getElementById("del");
 const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const product = document.getElementById("product");
 const division = document.getElementById("division");
 
 numBtn.forEach(button => button.addEventListener('click', function(result) {
-  // If operator value is empty, store number inputs into second number variable
+  // If operator value is empty, store number inputs into first number variable
   let populate = document.createElement('p');
   if (operateVal === "") {
+    //Prevents multiple decimals
     if (firstNum.includes(".") && button.innerText ===".") {
       return
     }
+    //Display on button press
     currentDisplay.innerHTML="";
     firstNum += button.innerText;
     populate.textContent = firstNum;
     currentDisplay.appendChild(populate);
     console.log(firstNum)
-    
-  //Else if operator value is pressed/has value, store data into second number variable
-  } else if (operateVal !== "") {
+
+  //If operator value is pressed and has value, store data into second number variable
+  } 
+  else if (operateVal !== "") {
     if (secNum.includes(".") && button.innerText ===".") {
       return
     }
@@ -84,15 +87,15 @@ numBtn.forEach(button => button.addEventListener('click', function(result) {
 }));
 
 
-//Toggle background color of operator button to indicate second variable is being recorded
+//Toggle background color of selected operator button
 operator.forEach(button => button.addEventListener('click', function(event, populate) {
   if (secNum !== "" && operateVal !== "") {
     operate();
   }
   resetToggle();
   operateVal = button.innerText;
-  button.style.backgroundColor="lightBlue";
-  console.log(operateVal)
+  operatorToggle ();
+  console.log(operateVal);
 }));
 
 //Clear button to reset calculator variables and button toggles
@@ -104,21 +107,78 @@ allClear.addEventListener('click', function() {
   resetToggle();
 });
 
-// operator button toggle
-function resetToggle() {
+//Operator button toggle effect reset
+function resetToggle () {
   plus.style.backgroundColor="white";
   minus.style.backgroundColor="white";
   product.style.backgroundColor="white";
   division.style.backgroundColor="white";
 }
 
-//del.addEventListener('click', function() {
+// Toggles operator button
+function operatorToggle () {
+  if (operateVal === "+") {
+    plus.style.backgroundColor="lightBlue";
+  } 
+  else if (operateVal === "-") {
+    minus.style.backgroundColor="lightBlue";
+  } 
+  else if (operateVal === "x") {
+    product.style.backgroundColor="lightBlue";
+  } 
+  else if (operateVal === "÷") {
+    division.style.backgroundColor="lightBlue";
+  } 
+}
 
-//});
+// Delete button functionality
+del.addEventListener('click', function(populate) {
+  // if first number is not empty and operator is empty, slice last character in string and display
+  if (firstNum !== "" && operateVal === "") {
+    currentDisplay.innerHTML=""
+    firstNum = firstNum.slice(0,-1);
+    currentDisplay.textContent = firstNum;
+    // if sliced to empty from one character, display 0
+    if (firstNum === "") {
+      currentDisplay.textContent = "0";
+    }
+  } 
+  // if operator value is not empty and second value is, display first number and reset toggle
+  else if (operateVal !== "" && secNum === "") {
+    operateVal="";
+    currentDisplay.textContent = firstNum;
+    resetToggle();
+  // second number
+  } else if (operateVal !== "" && secNum !== "" ) {
+    currentDisplay.innerHTML=""
+    secNum = secNum.slice(0,-1);
+    currentDisplay.textContent = secNum;
+    // if second number is empty, go back a step to display first num and toggled operator
+    if (secNum === "") {
+      currentDisplay.textContent = firstNum;
+      operatorToggle();
+    }
+  }
+});
 
 //Clicks equal button and calls operate function
-equals.addEventListener('click', operate);
+equals.addEventListener('click', function () {
+  if (operateVal ==="" || secNum==="") {
+    return;
+  } 
+  else operate ();
+  } 
+);
+
+// next steps
+// delete button
+// positive negative buttons (multiply by -1)
+// Devide by 0 interactions
+// Round?
+// sync to keyboard
+// make grid even
+// decorate css
 
 
-// tasks: string together calculations without equals button
-// clicking a new number starts a new calculation instead of adding to result string
+// equal buttons interactions
+// Clear button
